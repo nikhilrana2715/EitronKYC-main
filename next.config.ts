5f39ec8470b26f9d7bc33b7ced62d4a393af2b9a
Nikhil Rana <204745055+nikhilrana2715@users.noreply.github.com>
2026-04-08 06:42:27 +0530

    fix next.config for next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Next.js nodemon
   reactStrictMode: false,
-  webpack: (config, { dev }) => {
-    if (dev) {
-      // webpack
-      config.watchOptions = {
-        ignored: ['**/*'],
-      }
-    }
-    return config;
-  },
-  eslint: {
-    // ESLint
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
